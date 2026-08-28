b0df2f27365d0896e3a4ebb4dcd6d1ad2c25001d
kjura <30084359+kjura@users.noreply.github.com>
2026-08-28 17:26:04 +0200

    scripts: add command line arg support for python script

--- script_pytube.py
+++ script_pytube.py
@@ -1,6 +1,6 @@
 import yt_dlp
 from yt_dlp.utils import download_range_func
-
+import sys
 
 def download_audio(link):
   with yt_dlp.YoutubeDL({'extract_audio': True, 'format': 'bestaudio', 'outtmpl': '%(title)s.mp3'}) as video:
@@ -9,5 +9,8 @@ def download_audio(link):
     print(video_title)
     video.download(link)    
 
-YT_LINK = "https://www.youtube.com/watch?v=29L01vKrAzA"
+if len(sys.argv) != 2:
+  raise ValueError(f"script_pytube.py must take exactly one argument YT_LINK, but were provided {sys.argv[1:]}")
+
+YT_LINK = sys.argv[1]
 download_audio(YT_LINK)
